2fd32b92836fe04680c5657771812141b6f1c3fb
Savannah Greuel <smallidgesavannah@gmail.com>
2019-05-11 05:53:47 -0600

    file structure set up

--- shopmate/src/App.js
+++ shopmate/src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <header></header>
+      <nav></nav>
+      <div className = "pageContainer">
+        <div className = "attnCatcherContainer">
+          <h1 className = " attnCatcherHeader"> </h1>
+          <ul className = "categories">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div> {/* end of attnCatcherContainer*/}
+        <div className = "topBodyContainer">
+          <div className = "filerBar">
+
+          </div> {/*end of filterBar */}
+          <div className = "productContainer1">
+            <div className = "product">
+
+            </div>{/*end of product */}
+
+          </div>{/*end of productContainer1 */}
+        </div>{/*end of topBodyContainer */}
+        <div className = "bottomBodyContainer">
+          <div className = "productContainer2">
+            <div className = "product">
+
+            </div>{/*end of product */}
+          </div>{/*end of productcontainer2 */}
+          <div className = "bottomAttnCatcher">
+            <h1 className = " bottomattnCatcherHeader"> </h1>
+            <p className = "tagline"></p>
+            <button className = "whiteButton"></button>
+          </div> {/*end bottomAttnCatcher*/}
+          <div className = "subscribe">
+    
+          </div>{/*end of subscribe */}
+        </div>{/*end of bottomBodyContainer */}
+      
+      </div> {/* end of pageContainer*/}
+      <footer></footer>
+    </div> //end of app
   );
 }
 
